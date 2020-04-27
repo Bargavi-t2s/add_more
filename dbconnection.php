4173bfb827e04c190fe94b167ce8a36987b35050
Bargavi-t2s <59874562+Bargavi-t2s@users.noreply.github.com>
2020-04-27 11:10:54 +0530

    Delete dbconnection.php

--- dbconnection.php
+++
@@ -1,9 +0,0 @@
-<?php
-$db_name = "bartestdb"; // database name
-$db_user = "bargavi"; // database username
-$db_pass = "manickam"; // database password
-$db_host = "localhost"; // 
-$error   = "";
-$db      = "";
-$db = mysqli_connect("$db_host", "$db_user", "$db_pass", "$db_name");
-?>
